04cd188ed4281293735cc5acd2b298c89721fc76
Dimael Rivas Chavez <88595171+artrivas@users.noreply.github.com>
2023-09-24 22:03:06 -0500

    Update AVL.h

--- avl/src/AVL.h
+++ avl/src/AVL.h
@@ -10,7 +10,7 @@
 #include<vector>
 #include<cstdio>
 #include<cstring>
-#include "Record.h"
+#include "./records/Record.h"
 #include<functional>
 using namespace std;
 template<typename KeyType,int attrpos>
