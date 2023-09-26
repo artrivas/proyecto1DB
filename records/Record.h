5108bce2dd2eefd739725d337d5d747ec648b64b
artrivas <rivaschavezd5k@gmail.com>
2023-09-26 11:47:09 -0500

    ligero detalle

--- records/Record.h
+++ records/Record.h
@@ -12,9 +12,9 @@ struct Record{
     char fname[20]{};
     char lname[20]{};
     char position[20]{};
-    char height[5]{};
+    char height[15]{};
     int weight;
-    char birthday[11]{};
+    char birthday[15]{};
     char country[15]{};
     char school[25]{};
     int draft_year;
@@ -22,7 +22,7 @@ struct Record{
     int draft_number;
     Record();
     void print_vals();
-    void set_vals();
+    void set_vals(long pid,char fn[],char ln[], char pos[],char h[], int w, char birth[],char cou[], char scho[],int dy,int dr,int dn);
     vector<long> get_prefix();
 };
 
